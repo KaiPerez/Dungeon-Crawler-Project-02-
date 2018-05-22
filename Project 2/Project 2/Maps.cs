84123be5921a66ac98780e22075184cfecd82fc8
KaiPerez <Hillybillykai@gmail.com>
2018-05-22 08:53:19 -0600

    New project

--- Project 2/Project 2/Maps.cs
+++ Project 2/Project 2/Maps.cs
@@ -6,39 +6,6 @@ using System.Threading.Tasks;
 
 namespace Project_2
 {
-    class Maps
-    {
-        public bool GoNorth;
-        public bool GoSouth;
-        public bool GoWest;
-        public bool GoEast;
-        public bool IsMonster;
-        public bool IsLoot;
-        public bool IsSecret;
 
-        public Room(bool N, bool S, bool W, bool E, bool IsM, bool IsL, bool IsS)
-        {
-            GoNorth = N;
-            GoSouth = S;
-            GoWest = W;
-            GoEast = E;
-            IsMonster = IsM;
-            IsLoot = IsL;
-            IsSecret = IsS;
-        }
-        
-       public bool RoomAndStuff()
-        {
-            if (GoNorth == true)
-            {
-                return true;
-            } else
-            {
-                return false;
-            }
-        }
-
-
-    }
 }
 
