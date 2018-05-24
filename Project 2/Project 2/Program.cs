0d719b7ae7d37a456364f5422d438c8f8b77a564
KaiPerez <Hillybillykai@gmail.com>
2018-05-24 08:37:43 -0600

    Program is done.

--- Project 2/Project 2/Program.cs
+++ Project 2/Project 2/Program.cs
@@ -35,9 +35,76 @@ namespace Project_2
             };
  
             PrintBoard(board);
-        
-            Console.WriteLine("Enter a direction.");
-            board[2][2] = player;
+            int x = 2;
+            int y = 2;
+          
+            Console.WriteLine("Enter a direction To Move");
+            Console.WriteLine("(Up, Down, Left, Right)");
+            Console.WriteLine("Enter 'Exit' to leave the program.");
+            string Direction = "Nothing";
+            while (Direction != "Exit") {
+                Console.WriteLine("Enter a Direction");
+                Direction = Console.ReadLine();
+                if (Direction == "Up" || Direction == "up")
+                {
+                    if (board[y - 1][x] == ".")
+                    {
+
+                        board[y][x] = ".";
+                        y = y - 1;
+                        board[y][x] = "V";
+                        PrintBoard(board);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Can't go there!");
+                    }
+                } else if (Direction == "Down" || Direction == "down")
+                {
+                    if (board[y + 1][x] == ".")
+                    {
+
+                        board[y][x] = ".";
+                        y = y + 1;
+                        board[y][x] = "V";
+                        PrintBoard(board);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Can't go there!");
+                    }
+                }
+                else if (Direction == "Right" || Direction == "right")
+                {
+                    if (board[y][x + 1] == ".")
+                    {
+
+                        board[y][x] = ".";
+                        x = x + 1;
+                        board[y][x] = "V";
+                        PrintBoard(board);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Can't go there!");
+                    }
+                }
+                else if (Direction == "Left" || Direction == "left")
+                {
+                    if (board[y][x - 1] == ".")
+                    {
+
+                        board[y][x] = ".";
+                        x = x - 1;
+                        board[y][x] = "V";
+                        PrintBoard(board);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Can't go there!");
+                    }
+                }
+            }
 
             Console.ReadKey();
            
